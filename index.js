2ba13daea01ee5ae75f49bf79bd7b0e22793466f
Marcio de Araujo Padilha <marcio.de.a.padilha@accenture.com>
2018-01-26 09:05:18 -0300

    mohammed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ bye: "little gilsen" });
+  res.send({ bye: "Mohamed" });
 });
 
 const PORT = process.env.PORT || 5000;
